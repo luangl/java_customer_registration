c0616a63050a2441b6e46df07172fb8dd09b5b4d
Luan Glaab <luanf.glaab@gmail.com>
2023-07-11 23:04:37 -0300

    Upload Arquivos

---
+++ src/br/com/luan/dao/IClienteDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.luan.dao;
+
+import br.com.luan.domain.Cliente;
+
+;import java.util.Collection;
+
+/**
+ *
+ * @author rodrigo.pires
+ */
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
